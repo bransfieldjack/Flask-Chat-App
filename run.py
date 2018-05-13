1ba6f77c64568d74f45733598f09b5b6558b1475
Jack Bransfield <bransfieldjack@gmail.com>
2018-05-13 22:30:01 +0000

    Functionality to write to/read from a list.

--- run.py
+++ run.py
@@ -1,8 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+messages = []
 
+def add_messages(username, message):
+    """
+    Add messages to the 'messages' list
+    """
+    messages.append("{}: {}".format(username, message))
+    
+
+def get_all_messages():
+    """
+    Get all of the messages and seperate them using a 'br'
+    """
+    return "<br>".join(messages)
+    
 
 @app.route('/')
 def index():
@@ -14,12 +28,19 @@ def index():
     
 @app.route('/<username>')
 def user(username):
+    """
+    Display chat messages
+    """
     return "Hi " + username
     
     
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    return "{0}: {1}".format(username, message)
+    """
+    Create a new message and redirect back to the chat page
+    """
+    add_messages(username, message)
+    return redirect(username)
     
     
 app.run(host=os.getenv('IP'), port = int(os.getenv('PORT')), debug = True)
