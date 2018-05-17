8eeb900057d840ccbc8dbb6a27fedd93729cd264
Jack Bransfield <bransfieldjack@gmail.com>
2018-05-17 21:55:22 +0000

    Updated the format/display of the chat message output.

--- run.py
+++ run.py
@@ -1,46 +1,45 @@
 import os
-from flask import Flask, redirect
+from datetime import datetime
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 messages = []
 
+
 def add_messages(username, message):
-    """
-    Add messages to the 'messages' list
-    """
-    messages.append("{}: {}".format(username, message))
-    
+    """Add messages to the `messages` list"""
+    now = datetime.now().strftime("%H:%M:%S")
+    messages_dict = {"timestamp": now, "from": username, "message": message}
+    messages.append(messages_dict)
+
 
 def get_all_messages():
-    """
-    Get all of the messages and seperate them using a 'br'
-    """
-    return "<br>".join(messages)
-    
+    """Get all of the messages and separate them by a `br`"""
+    return messages
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    """
-    Main page with instructions. 
-    """
-    return "To send a message use /USERNAME/MESSAGE"
-    
-    
+    """Main page with instructions"""
+    if request.method == "POST":
+        with open("data/users.txt", "a") as user_list:
+            user_list.write(request.form["username"] + "\n")
+        return redirect(request.form["username"])
+    return render_template("index.html")
+
+
 @app.route('/<username>')
 def user(username):
-    """
-    Display chat messages
-    """
-    return "<h1>Welcome, {0}</h1>{1}".format(username, get_all_messages())
-    
-    
+    """Display chat messages"""
+    messages = get_all_messages()
+    return render_template("chat.html", username=username, chat_messages=messages)
+
+
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    """
-    Create a new message and redirect back to the chat page
-    """
+    """Create a new message and redirect back to the chat page"""
     add_messages(username, message)
     return redirect(username)
-    
-    
-app.run(host=os.getenv('IP'), port = int(os.getenv('PORT')), debug = True)
+
+
+app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
