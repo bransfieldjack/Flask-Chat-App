7bd767cee6261c879cf1e3b48e1aa3e18ac5d676
Jack Bransfield <bransfieldjack@gmail.com>
2018-05-20 03:23:15 +0000

    Code refactor .1

--- run.py
+++ run.py
@@ -3,41 +3,62 @@ from datetime import datetime
 from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
-messages = []
 
+
+def write_to_file(filename, data):
+    """
+    Handle the process of writing data to a file
+    """
+    with open(filename, "a") as file:
+        file.writelines(data)
 
 def add_messages(username, message):
-    """Add messages to the `messages` list"""
-    now = datetime.now().strftime("%H:%M:%S")
-    messages_dict = {"timestamp": now, "from": username, "message": message}
-    messages.append(messages_dict)
+    """
+    Add messages to the `messages` list
+    """
+    write_to_file("data/messages.txt", "({0}) {1} - {2}\n".format(
+              datetime.now().strftime("%H:%M:%S"), 
+              username.title(), 
+              message))
 
 
 def get_all_messages():
-    """Get all of the messages and separate them by a `br`"""
+    """
+    Get all of the messages and separate them by a `br`
+    """
+    messages = []
+    with open("data/messages.txt", "r") as chat_messages:
+        messages = chat_messages.readlines()
     return messages
 
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    """Main page with instructions"""
+    """
+    Main page with instructions
+    """
+    
+    #Handle POST request
     if request.method == "POST":
-        with open("data/users.txt", "a") as user_list:
-            user_list.write(request.form["username"] + "\n")
+        write_to_file("data/users.txt", request.form["username"] + "\n")
         return redirect(request.form["username"])
     return render_template("index.html")
 
 
 @app.route('/<username>')
 def user(username):
-    """Display chat messages"""
+    """
+    Display chat messages
+    """
     messages = get_all_messages()
     return render_template("chat.html", username=username, chat_messages=messages)
 
 
 @app.route('/<username>/<message>')
 def send_message(username, message):
-    """Create a new message and redirect back to the chat page"""
+    """
+    Create a new message and redirect back to the chat page
+    """
     add_messages(username, message)
     return redirect(username)
 
